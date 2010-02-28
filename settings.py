3742a7def67a0c506f1440165066ae91f15bc233
davis <davis@maraudertech.com>
2010-02-28 16:24:42 +0000

    deleting top level templates

--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
@@ -69,6 +73,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'vidconvert','templates')
 )
 
 INSTALLED_APPS = (
